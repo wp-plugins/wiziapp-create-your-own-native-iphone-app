bcce64ac7dc4a22308b5b9e2bf885466c26b3ec8
wiziapp <wiziapp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-10 17:37:22 +0000

    Try to fix
    git-svn-id: https://plugins.svn.wordpress.org/wiziapp-create-your-own-native-iphone-app/tags/v2.0.2a@650927 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/classes/WiziappAdminDisplay.php
+++ includes/classes/WiziappAdminDisplay.php
@@ -40,6 +40,8 @@ class WiziappAdminDisplay {
 				add_menu_page('WiziApp', 'WiziApp', 'administrator', 'wiziapp', array('WiziappPostInstallDisplay', 'display'), $iconPath);
 			} elseif ( $installer->needUpgrade() ){
 				add_menu_page('WiziApp', 'WiziApp', 'administrator', 'wiziapp', array('WiziappUpgradeDisplay', 'display'), $iconPath);
+			} elseif ( ! WiziappConfig::getInstance()->webapp_installed && ! WiziappConfig::getInstance()->skip_reload_webapp ){
+				add_menu_page('WiziApp', 'WiziApp', 'administrator', 'wiziapp', array('WiziappWebappDisplay', 'display'), $iconPath);
 			} elseif ($configured === FALSE){
 				if (isset($_GET['wiziapp_reload_webapp'])) {
 					add_menu_page('WiziApp', 'WiziApp', 'administrator', 'wiziapp', array('WiziappWebappDisplay', 'display'), $iconPath);
@@ -47,8 +49,6 @@ class WiziappAdminDisplay {
 				else {
 					add_menu_page('WiziApp', 'WiziApp', 'administrator', 'wiziapp', array('WiziappGeneratorDisplay', 'display'), $iconPath);
 				}
-			} elseif ( ! WiziappConfig::getInstance()->webapp_installed && ! WiziappConfig::getInstance()->skip_reload_webapp ){
-				add_menu_page('WiziApp', 'WiziApp', 'administrator', 'wiziapp', array('WiziappWebappDisplay', 'display'), $iconPath);
 			} else {
 				// We are installed and configured
 				// add_submenu_page('wiziapp', __('dashboard'), __('dashboard'), 'administrator', 'wiziapp_dashboard_display', 'wiziapp_dashboard_display');
