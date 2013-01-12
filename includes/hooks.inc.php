b2bf114b4ee9cd35295e6c68f452a1f517836133
wiziapp <wiziapp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-12 07:47:46 +0000

    Try to deactivate the Webapp feature on the "Wiziapp plugin upgrade" process
    git-svn-id: https://plugins.svn.wordpress.org/wiziapp-create-your-own-native-iphone-app/tags/v2.0.2a@651569 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/hooks.inc.php
+++ includes/hooks.inc.php
@@ -14,6 +14,9 @@
 function wiziapp_attach_hooks(){
 	$ce = new WiziappContentEvents();
 
+	// Deactivate the Webapp feature on the "Wiziapp plugin upgrade" process
+	WiziappWebappDisplay::deactivate_on_upgrade();
+
 	//add_action('admin_menu', 'wiziapp_setup_menu');
 	add_action( 'admin_menu', array( 'WiziappAdminDisplay', 'setup' ) );
 
