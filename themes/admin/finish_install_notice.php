f95cad1462477bbeea4947fd04e9018fdeed7dba
wiziapp <wiziapp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-03 12:36:31 +0000

    New revamped version
    git-svn-id: https://plugins.svn.wordpress.org/wiziapp-create-your-own-native-iphone-app/trunk@868528 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- themes/admin/finish_install_notice.php
+++
@@ -1,4 +0,0 @@
-<div id="wiziapp_active_notice" class="updated">
-	<div></div>
-	<div></div>
-</div>
--- themes/admin/finish_upgrade_notice.php
+++
@@ -1,5 +0,0 @@
-<div class="updated fade">
-	<p class="wiziapp_notice_mode">
-		<?php echo WiziappAdminNotices::$_notices_texts['upgrade']['content']; ?>
-	</p>
-</div>
