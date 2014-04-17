17a645d5ce51aa4522ac6450e549ba4d8e4470fc
wiziapp <wiziapp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-17 16:03:44 +0000

    Release version 3.1.0
    git-svn-id: https://plugins.svn.wordpress.org/wiziapp-create-your-own-native-iphone-app/tags/3.1.0@896405 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ modules/monetization.php
@@ -0,0 +1,36 @@
+<?php
+	require_once(dirname(dirname(__FILE__)).'/includes/settings.php');
+	require_once(dirname(dirname(__FILE__)).'/includes/hook.php');
+	require_once(dirname(dirname(__FILE__)).'/includes/purchase_hook.php');
+
+	class WiziappPluginModuleMonetization
+	{
+		function init()
+		{
+			$hook = new WiziappPluginPurchaseHook();
+			$hook->hook('ads', '/ads', array(&$this, '_licensed'), array(&$this, '_analytics'));
+		}
+
+		function _licensed()
+		{
+?>
+					<script type="text/javascript">
+						if (window.parent && window.parent.jQuery) {
+							window.parent.jQuery("#wiziapp-plugin-admin-settings-box-monetization-body-buy").removeClass("wiziapp-plugin-admin-settings-box-body-active");
+							window.parent.jQuery("#wiziapp-plugin-admin-settings-box-monetization-body").addClass("wiziapp-plugin-admin-settings-box-body-active");
+						}
+						if (window.parent && window.parent.tb_remove) {
+							window.parent.tb_remove();
+						}
+					</script>
+<?php
+		}
+
+		function _analytics()
+		{
+			return '/ads/purchased';
+		}
+	}
+
+	$module = new WiziappPluginModuleMonetization();
+	$module->init();
